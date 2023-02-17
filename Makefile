2ef2899cebe98fbe8988e96bf47d3cbae084006b
unee0x <unee0x@gmail.com>
2023-02-17 05:30:32 -0500

    funcs added

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 KMOD=mypci
-SRCS=mypci.c
+SRCS=mypci.c device_if.h bus_if.h pci_if.h
 
 .include <bsd.kmod.mk>
--- mypci.c
+++ mypci.c
@@ -25,8 +25,107 @@ static d_write_t   mypci_write;
 static struct cdevsw mypci_cdevsw = {
     .d_version = D_VERSION,
     .d_open = mypci_open,
+    .d_read = mypci_read,
     .d_close = mypci_close,
     .d_write = mypci_write,
     .d_name = "mypci"
     };
 
+static devclass_t mypci_devclass;
+
+static int
+mypci_open( struct cdev *dev, int oflags, int devtype, struct thread *td)
+{
+  struct mypci_softc *sc;
+  sc = dev->si_drv1;
+  device_printf( sc->mydev, "Opened Successfully\n");
+  return (0);
+}
+
+static int                                                                        
+mypci_close( struct cdev *dev, int fflags, int devtype, struct thread *td)         
+{                                                                                
+  struct mypci_softc *sc;                                                        
+  sc = dev->si_drv1;                                                             
+  device_printf( sc->mydev, "Closed Successfully\n");                              
+  return (0);                                                                    
+}
+
+static int
+mypci_read( struct cdev *dev, struct uio *uio, int ioflag ){
+  struct mypci_softc *sc;
+  sc = dev->si_drv1;
+  device_printf( sc->mydev, "Read request = %zd\n", uio->uio_resid );
+  return (0);
+}
+
+
+static int                                                                       
+mypci_write( struct cdev *dev, struct uio *uio, int ioflag ){                    
+  struct mypci_softc *sc;                                                        
+  sc = dev->si_drv1;                                                             
+  device_printf( sc->mydev, "Write request = %zd\n", uio->uio_resid );             
+  return (0);                                                                    
+}
+
+static struct _pcsid {
+  uint32_t    type ;
+  const char  *desc;
+} pci_ids[] = {{0x1234abcd, "Red PCI Widget"},
+               {0x4321fedc, "Blue PCI Widget"},
+               {0x00000000, NULL}};
+
+
+static int
+mypci_probe( device_t dev )
+{
+  uint32_t type = pci_get_devid(dev);
+  struct _pcsid *ep = pci_ids;
+
+  while( ep->type && ep->type != type )
+    ep++;
+  if( ep->desc ) {
+    device_set_desc( dev, ep->desc );
+    return (BUS_PROBE_DEFAULT);
+  }
+
+  return (ENXIO);
+}
+
+static int
+mypci_attach( device_t dev )
+{
+  struct mypci_softc *sc = device_get_softc( dev );
+  int unit = device_get_unit( dev );
+  sc->cdev = make_dev(&mypci_cdevsw, unit, UID_ROOT, GID_WHEEL,
+		       0600, "mypci%d", unit);
+  sc->cdev->si_drv1 = sc;
+
+  return (0);
+}
+
+static int
+mypci_detach( device_t dev )
+{
+  struct mypci_softc *sc = device_get_softc( dev );
+
+  destroy_dev( sc->cdev );
+  return (0);
+}
+
+static device_method_t mypci_methods[] = {
+  /* Device Interface */
+
+  DEVMETHOD( device_probe, mypci_probe ),
+  DEVMETHOD( device_attach, mypci_attach ),
+  DEVMETHOD( device_detach, mypci_detach ),
+  { 0, 0 }
+};
+
+static driver_t mypci_driver = {
+  "mypci",
+  mypci_methods,
+  sizeof(struct mypci_softc)
+};
+
+DRIVER_MODULE( mypci, pci, mypci_driver, mypci_devclass, 0, 0 );
